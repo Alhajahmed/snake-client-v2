37d3c30d13ac5f860fc63f3a0ff6d6c88b023e80
Ahmed <ahmed91alhaj@gmail.com>
2023-04-23 20:48:17 -0400

    Store keys into an object

--- constants.js
+++ constants.js
@@ -1,7 +1,15 @@
 const IP = "localhost";
 const PORT = 50541;
 
+const movmentKeys = {
+  w: "Move: up",
+  a: "Move: left",
+  s: "Move: down",
+  d: "Move: right",
+  v: "Say: The winner! wohoo!",
+};
 module.exports = {
   IP,
   PORT,
+  movmentKeys,
 };
--- input.js
+++ input.js
@@ -1,3 +1,4 @@
+const { KEYMAPPINGS, movmentKeys } = require("./constants");
 // Stores the active TCP connection object.
 let connection;
 
@@ -5,17 +6,9 @@ const handleUserInput = function (data) {
   if (data === "\u0003") {
     console.log("GAMEOVER");
     process.exit();
-  } else if (data === "w") {
-    connection.write("Move: up");
-    console.log("up");
-  } else if (data === "a") {
-    connection.write("Move: left");
-  } else if (data === "s") {
-    connection.write("Move: down");
-  } else if (data === "d") {
-    connection.write("Move: right");
-  } else if (data === "v") {
-    connection.write("Say: I did it! Whohoo!");
+  }
+  if (movmentKeys[data]) {
+    connection.write(movmentKeys[data]);
   }
 };
 
